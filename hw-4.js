71d90cb461484e7d177f842025b23230508c6f97
Anastasija042 <anastasija_042@mail.ru>
2024-02-23 16:27:36 +0200

    corr

--- hw-4.js
+++ hw-4.js
@@ -1,10 +1,9 @@
-/*//Задание 1
+//Задание 1
 let i = 0;
 while (i < 2) {
   console.log("Привет!");
   i++;
 }
-
 
 //Задание 2
 let i = 1;
@@ -22,12 +21,11 @@ do {
 
 //Задание 4
 const obj = {
-    "Коля" : '200',
-    "Вася" : '300',
-    "Петя" : '400'
+  Коля: "200",
+  Вася: "300",
+  Петя: "400",
 };
 for (let key in obj) console.log(`${key} - зарплата ${obj[key]} долларов.`);
-
 
 //Задание 5
 
@@ -39,14 +37,13 @@ while (n >= 50) {
 }
 console.log(n);
 console.log(num);
-*/
 
 //Задание 6
 let firstFriday = 5;
-for (let day = 1; day <= 31; day++) {
-  if (((day - 1) % 7) + 1 === firstFriday) {
-    console.log(
-      "Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет."
-    );
-  }
+let day = firstFriday;
+while (day <= 31) {
+  console.log(
+    "Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет."
+  );
+  day += 7;
 }
